91375ff32f287f541a8749ce66a09b121d148c50
Sion Dawson <siondawson91@gmail.com>
2024-11-12 15:43:42 +0000

    update app.py to use production and developmemt variables

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os
 from samantha_cello import app
 
 if __name__ == "__main__":
+    environment = os.getenv("ENVIRONMENT", "production")
+    debug_mode = environment == "development"
+
     app.run(
         host=os.getenv("IP", "0.0.0.0"),
         port=int(os.getenv("PORT", 5000)),
-        debug=bool(os.getenv("DEBUG", True))
+        debug=debug_mode
     )
