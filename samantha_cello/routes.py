42da82a28b636fa99b4652f3e675466a3656cc90
Sion Dawson <siondawson91@gmail.com>
2024-11-27 17:53:21 +0000

    add custom 404 page

--- samantha_cello/routes.py
+++ samantha_cello/routes.py
@@ -361,3 +361,9 @@ def generate_sitemap():
     return ''.join(sitemap_xml)
 
 
+# Error handler for 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
